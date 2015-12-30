76f7d7cd088a3dfe1b8e5fb42e7397693008b0ba
E-Xor <e-xormail@tut.by>
2015-12-30 14:34:12 -0500

    Made it run, but not recommend

--- recommender.rb
+++ recommender.rb
@@ -1,19 +1,38 @@
-#!/usr/bin/env ruby
+include Java
+$CLASSPATH << "jars/postgresql-9.4.1207.jre7.jar"
+
+require 'jruby_mahout'
+
+puts "START"
 
 recommender = JrubyMahout::Recommender.new(
-    "PearsonCorrelationSimilarity",
-    5,
+    "LogLikelihoodSimilarity",
+    3,
     "GenericUserBasedRecommender",
     false)
 
-# recommender.data_model = JrubyMahout::DataModel.new(
-#    "file",
-#     { :file_path => "data.csv" }).data_model
-# Your data will have to be in the following format:
-#
-# 1,3,5
-# 1,2,1
-# 2,3,5
+recommender.data_model = JrubyMahout::DataModel.new(
+   "file",
+    { :file_path => "recommender_data.csv" }).data_model
 
-puts recommender.recommend(2, 10, nil)
+puts "RECOMMEND"
 
+puts recommender.recommend(4, 3, nil)
+
+puts "EVALUATE"
+
+puts recommender.evaluate(0.7, 0.3)
+
+puts "SIMILLAR USERS"
+
+puts recommender.similar_users(1, 5, nil)
+
+puts "BECAUSE"
+
+puts recommender.recommended_because(1, 138, 5)
+
+puts "PREFERENCE"
+
+puts recommender.estimate_preference(1, 138)
+
+puts "END"
