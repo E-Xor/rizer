4c1a9230efc7957f7deeaa6d393a74418a4c00bd
E-Xor <e-xormail@tut.by>
2016-08-21 07:36:08 -0400

    Separate tables

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem "jruby_mahout"
 gem 'activerecord', '~> 4.2.6'
-gem 'activerecord-jdbc-adapter', '~> 1.3.20'
-gem 'jdbc-mysql', '~> 5.1.38'
 gem 'activerecord-jdbcmysql-adapter', '~> 1.3.20'
+gem 'activerecord-jdbc-adapter', '~> 1.3.20' # optional?
+gem 'jdbc-mysql', '~> 5.1.38'
 gem 'rison', '~> 2.0.0'
--- recommender/recommender_from_db.rb
+++ recommender/recommender_from_db.rb
@@ -33,15 +33,6 @@ java_import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommen
 
 java_import org.apache.mahout.cf.taste.common.Weighting
 
-# java_import org.apache.logging.log4j
-# require "/usr/share/java/log4j-1.2.jar"
-
-# log4j.rootLogger=INFO, stdout
-# log4j.appender.stdout=org.apache.log4j.ConsoleAppender
-# log4j.appender.stdout.layout=org.apache.log4j.PatternLayout
-# log4j.appender.stdout.layout.ConversionPattern="%d [%t] %-5p %c - %m%n"
-# setLogWriter?
-
 puts "MySQL"
 java_import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLJDBCDataModel
 java_import org.apache.mahout.cf.taste.impl.model.jdbc.ReloadFromJDBCDataModel
@@ -50,7 +41,8 @@ java_import org.apache.mahout.cf.taste.impl.model.jdbc.ReloadFromJDBCDataModel
 # export CLASSPATH
 java_import com.mysql.jdbc.jdbc2.optional.MysqlDataSource
 
-db_config = YAML::load(IO.read('config/database.yml'))
+db_config = YAML::load(IO.read("#{File.expand_path(File.dirname(__FILE__))}/../config/database.yml"))
+
 data_source = MysqlDataSource.new()
 data_source.setUser(db_config['username'])
 data_source.setPassword(db_config['password'])
@@ -58,65 +50,123 @@ data_source.setServerName(db_config['host'])
 data_source.setPortNumber(db_config['port'])
 data_source.setDatabaseName(db_config['database'])
 
-# # require 'yaml'
-# # require 'active_record'
-# # require 'bundler/setup'
-# # require 'rison'
+require 'yaml'
+require 'active_record'
+require 'bundler/setup'
+# require 'rison'
 
-# # puts "Connecting to DB..."
-# # db_config = YAML::load(IO.read('config/database.yml'))
-# # ActiveRecord::Base.establish_connection(db_config)
+puts "Connecting to DB..."
+# db_config = YAML::load(IO.read('config/database.yml'))
+ActiveRecord::Base.establish_connection(db_config)
 
-# # ActiveRecord::Base.connection.execute('
-# #   DROP VIEW v_recommender_data_source;
-# # ')
-# # ActiveRecord::Base.connection.execute('
-# #   CREATE VIEW v_recommender_data_source AS
-# #   SELECT user_id, profile_id, COUNT(*) AS rating FROM profile_views
-# #   WHERE user_id IS NOT NULL AND profile_id > 0
-# #   GROUP BY CONCAT(user_id, '_', profile_id) ORDER BY rating DESC;
-# # ')
+ActiveRecord::Base.connection.execute('
+  DROP VIEW v_recommender_media_brand;
+  DROP VIEW v_recommender_media_company;
+  DROP VIEW v_recommender_media_agency;
+  DROP VIEW v_recommender_agency_brand;
+  DROP VIEW v_recommender_agency_company;
+  DROP VIEW v_recommender_agency_agency;
+')
 
-data_model = ReloadFromJDBCDataModel.new(MySQLJDBCDataModel.new(data_source, "v_recommender_data_source", "user_id", "profile_id", "rating", nil))
+def v_recomender_sql(client_type: :media, profile_type: :brand, days_ago: 30)
+  
+  case profile_type
+  when :company
+    join_table = 'imp_company'
+    join_id = 'company_id'
+  when :brand
+    join_table = 'imp_company'
+    join_id = 'company_id'
+  when :agency
+    join_table = 'imp_company'
+    join_id = 'company_id'
+  end
 
-# puts "SIMILLAR ITEMS"
-# similarity = PearsonCorrelationSimilarity.new(data_model)
-# recommender = GenericItemBasedRecommender.new(data_model, similarity)
-# similarItems = recommender.mostSimilarItems(1602, 10, nil) # Items simillar to item <FIRST ARG>. Return <SECOND ARG> items
-# similarItems.each{|s| print "#{s.getItemID},"}; puts;
+  if join_table && join_id
+    profile_type_subquery = "INNER JOIN tlo_dev.#{join_table} jt ON (jt.#{join_id}=p.profile_id)"
+  end
 
-# similarity = PearsonCorrelationSimilarity.new(data_model)
-# # similarity = TanimotoCoefficientSimilarity.new(data_model)
-# neighborhood = NearestNUserNeighborhood.new(20, similarity, data_model) # <FIRST ARG> is nearest N users to a given user.
-# recommender = GenericUserBasedRecommender.new(data_model, neighborhood, similarity)
 
-# puts "RECOMMEND"
+  client_type_subquery = ''
+  case client_type
+  when :media
+    client_type_subquery = " AND user_id NOT IN (SELECT DISTINCT(user_id) FROM tlo_dev.fm_users INNER JOIN tlo_dev.fm_accounts USING(account_id) WHERE category='Agency')"
+  when :agency
+    client_type_subquery = " AND user_id     IN (SELECT DISTINCT(user_id) FROM tlo_dev.fm_users INNER JOIN tlo_dev.fm_accounts USING(account_id) WHERE category='Agency')"
+  end
 
-# # puts recommender.recommend(46117, 10, nil) # Recoomend to user <FIRST ARG>. Number of recommendations - <SECOND ARG>
-# recommender.recommend(46117, 10, nil).each{|s| print "#{s.getItemID},"}; puts;
-# recommender.recommend(23221, 10, nil).each{|s| print "#{s.getItemID},"}; puts;
-# recommender.recommend(59210, 10, nil).each{|s| print "#{s.getItemID},"}; puts;
-# recommender.recommend(191814, 10, nil).each{|s| print "#{s.getItemID},"}; puts;
+   since_time_subquery = ''
+  if days_ago > 0
+    start_time  = (Time.now - days_ago.days).to_s(:db)
+    since_time_subquery = "AND p.created_at >= '#{start_time}'"
+  end
 
-# # 1 month
-# # separately company, brand, agency_id
-# # Load array from DB via AR or something
-# # Check dates in current algorythms and query same periods
-# # Figure out rating
-# # Run different algorythms for the same user, uniq results
-# # Log user and number of recommendations given
-# # Join profile names to test
+  sql_string = "
+    CREATE VIEW audit_dev.v_recommender_#{client_type}_#{profile_type} AS
+    SELECT user_id, profile_id, COUNT(*) AS rating FROM audit_dev.profile_views p
+    #{profile_type_subquery}
+    WHERE profile_id > 0
+    #{client_type_subquery}
+    #{since_time_subquery}
+    GROUP BY CONCAT(user_id, '_', profile_id) ORDER BY rating DESC;
+  "
+end
 
-# # SELECT v.*, c.company_name, a.agency_name, b.companybrand_name
-# # FROM `v_recommender_data_source` v
-# # LEFT JOIN tlo_dev.imp_company c ON (v.profile_id = c.company_id)
-# # LEFT JOIN tlo_dev.imp_company_brand b ON (v.profile_id = b.brand_id)
-# # LEFT JOIN tlo_dev.imp_agency a ON (v.profile_id = a.agency_id)
-# # WHERE v.user_id=59210;
+ActiveRecord::Base.connection.execute(v_recomender_sql(client_type: nil,     profile_type: nil,      days_ago: 0))
+ActiveRecord::Base.connection.execute(v_recomender_sql(client_type: :media,  profile_type: :company, days_ago: 0))
+ActiveRecord::Base.connection.execute(v_recomender_sql(client_type: :media,  profile_type: :brand,   days_ago: 0))
+ActiveRecord::Base.connection.execute(v_recomender_sql(client_type: :media,  profile_type: :agency,  days_ago: 0))
+ActiveRecord::Base.connection.execute(v_recomender_sql(client_type: :agency, profile_type: :company, days_ago: 0))
+ActiveRecord::Base.connection.execute(v_recomender_sql(client_type: :agency, profile_type: :brand,   days_ago: 0))
+ActiveRecord::Base.connection.execute(v_recomender_sql(client_type: :agency, profile_type: :agency,  days_ago: 0))
 
-# # SELECT company_name FROM tlo_dev.imp_company WHERE company_id IN (57437,22979,13085,296559,26261,6891,12513,16641,67209)
-# # UNION
-# # SELECT companybrand_name FROM tlo_dev.imp_company_brand WHERE brand_id IN (57437,22979,13085,296559,26261,6891,12513,16641,67209)
-# # UNION
-# # SELECT agency_name FROM tlo_dev.imp_agency WHERE agency_id IN (57437,22979,13085,296559,26261,6891,12513,16641,67209);
+abort
 
+# ActiveRecord::Base.connection.execute('
+  # CREATE VIEW v_recommender_data_source AS
+  # SELECT user_id, profile_id, COUNT(*) AS rating FROM profile_views
+  # WHERE user_id IS NOT NULL AND profile_id > 0
+  # GROUP BY CONCAT(user_id, '_', profile_id) ORDER BY rating DESC;
+# ')
+# Company separately
+# CREATE VIEW audit_dev.v_recommender_company AS
+# SELECT user_id, profile_id, COUNT(*) AS rating FROM audit_dev.profile_views p
+# INNER JOIN tlo_dev.imp_company c ON (c.company_id=p.profile_id)
+# WHERE user_id IS NOT NULL AND profile_id > 0
+# GROUP BY CONCAT(user_id, '_', profile_id) ORDER BY rating DESC;
+
+data_model = ReloadFromJDBCDataModel.new(MySQLJDBCDataModel.new(data_source, "v_recommender_company", "user_id", "profile_id", "rating", nil))
+
+puts "SIMILLAR ITEMS"
+similarity = PearsonCorrelationSimilarity.new(data_model)
+recommender = GenericItemBasedRecommender.new(data_model, similarity)
+similarItems = recommender.mostSimilarItems(1602, 10, nil) # Items simillar to item <FIRST ARG>. Return <SECOND ARG> items
+similarItems.each{|s| print "#{s.getItemID},"}; puts;
+
+similarity = PearsonCorrelationSimilarity.new(data_model)
+# similarity = TanimotoCoefficientSimilarity.new(data_model)
+neighborhood = NearestNUserNeighborhood.new(20, similarity, data_model) # <FIRST ARG> is nearest N users to a given user.
+recommender = GenericUserBasedRecommender.new(data_model, neighborhood, similarity)
+
+puts "RECOMMEND"
+
+# puts recommender.recommend(46117, 10, nil) # Recoomend to user <FIRST ARG>. Number of recommendations - <SECOND ARG>
+recommender.recommend(46117, 10, nil).each{|s| print "#{s.getItemID},"}; puts;
+recommender.recommend(23221, 10, nil).each{|s| print "#{s.getItemID},"}; puts;
+recommender.recommend(59210, 10, nil).each{|s| print "#{s.getItemID},"}; puts;
+recommender.recommend(191814, 10, nil).each{|s| print "#{s.getItemID},"}; puts;
+
+# separately company, brand, agency_id
+# separate user by pillar. example of TV for media and kids for agencies
+# include follows?
+# look at last 30 days
+# Run different algorythms for the same user, uniq results
+# Log user and number of recommendations given
+# Join profile names to test
+
+# Test
+# SELECT company_name,description FROM tlo_dev.imp_company WHERE company_id IN (8959,19944,6891,10524,643,3587,14955,3867,11833,4851);
+
+# SELECT DISTINCT(description), COUNT(*) c FROM tlo_dev.imp_company c
+# INNER JOIN audit_dev.profile_views p ON (c.company_id=p.profile_id)
+# WHERE p.user_id=191814 GROUP BY description ORDER BY c DESC;
